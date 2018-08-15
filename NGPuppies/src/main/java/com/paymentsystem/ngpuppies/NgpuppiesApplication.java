977d885f12546ec6b13cf3d0dfe0e30a7b6628d6
adriyanmihaylov <adriyanmihaylov@yahoo.com>
2018-08-15 08:42:47 +0300

    Initial commit

---
+++ NGPuppies/src/main/java/com/paymentsystem/ngpuppies/NgpuppiesApplication.java
@@ -0,0 +1,12 @@
+package com.paymentsystem.ngpuppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgpuppiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgpuppiesApplication.class, args);
+    }
+}
