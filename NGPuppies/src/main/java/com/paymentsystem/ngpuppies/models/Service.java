204b802cdde00e1a801ff616116b0808a072507e
qdrenchev <jany.drenchev@gmail.com>
2018-08-15 16:45:40 +0300

    added Service model

--- NGPuppies/src/main/java/com/paymentsystem/ngpuppies/models/Service.java
+++ NGPuppies/src/main/java/com/paymentsystem/ngpuppies/models/Service.java
@@ -1,4 +1,27 @@
 package com.paymentsystem.ngpuppies.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "services")
 public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "Type")
+    private String type;
+    public Service(){
+
+    }
+    public Service(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
