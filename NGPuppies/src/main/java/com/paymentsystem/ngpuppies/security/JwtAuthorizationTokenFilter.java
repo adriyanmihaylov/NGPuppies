40fe9f4a9f350400868a406859b73d30ee737ab1
adriyanmihaylov <adriyanmihaylov@yahoo.com>
2018-08-23 01:58:48 +0300

    Code refactored

--- NGPuppies/src/main/java/com/paymentsystem/ngpuppies/security/JwtAuthorizationTokenFilter.java
+++ NGPuppies/src/main/java/com/paymentsystem/ngpuppies/security/JwtAuthorizationTokenFilter.java
@@ -1,5 +1,6 @@
 package com.paymentsystem.ngpuppies.security;
 
+import com.sun.deploy.xml.BadTokenException;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,9 @@ public class JwtAuthorizationTokenFilter extends OncePerRequestFilter {
     private final JwtTokenUtil jwtTokenUtil;
     private final String tokenHeader;
 
-    public JwtAuthorizationTokenFilter(@Qualifier("jwtUserDetailsService") UserDetailsService userDetailsService, JwtTokenUtil jwtTokenUtil, @Value("${jwt.header}") String tokenHeader) {
+    public JwtAuthorizationTokenFilter(@Qualifier("jwtUserDetailsService") UserDetailsService userDetailsService,
+                                       JwtTokenUtil jwtTokenUtil,
+                                       @Value("${jwt.header}") String tokenHeader) {
         this.userDetailsService = userDetailsService;
         this.jwtTokenUtil = jwtTokenUtil;
         this.tokenHeader = tokenHeader;
@@ -42,37 +45,41 @@ public class JwtAuthorizationTokenFilter extends OncePerRequestFilter {
 
         String username = null;
         String authToken = null;
-        if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
-            authToken = requestHeader.substring(7);
-            try {
-                username = jwtTokenUtil.getUsernameFromToken(authToken);
-            } catch (IllegalArgumentException e) {
-                logger.error("an error occured during getting username from token", e);
-            } catch (ExpiredJwtException e) {
-                logger.warn("the token is expired and not valid anymore", e);
+        try {
+            if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
+                authToken = requestHeader.substring(7);
+                try {
+                    username = jwtTokenUtil.getUsernameFromToken(authToken);
+                } catch (IllegalArgumentException e) {
+                    logger.error("an error occured during getting username from token", e);
+                } catch (ExpiredJwtException e) {
+                    logger.warn("the token is expired and not valid anymore", e);
+                }
+            } else {
+                logger.warn("couldn't find bearer string, will ignore the header");
             }
-        } else {
-            logger.warn("couldn't find bearer string, will ignore the header");
-        }
 
-        logger.debug("checking authentication for user '{}'", username);
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            logger.debug("security context was null, so authorizating user");
+            logger.debug("checking authentication for user '{}'", username);
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                logger.debug("security context was null, so authorizating user");
 
-            // It is not compelling necessary to load the use details from the database. You could also store the information
-            // in the token and read it from it. It's up to you ;)
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                // It is not compelling necessary to load the use details from the database. You could also store the information
+                // in the token and read it from it. It's up to you ;)
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
-            // For simple validation it is completely sufficient to just check the token integrity. You don't have to call
-            // the database compellingly. Again it's up to you ;)
-            if (jwtTokenUtil.validateToken(authToken, userDetails)) {
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                logger.info("authorizated user '{}', setting security context", username);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                // For simple validation it is completely sufficient to just check the token integrity. You don't have to call
+                // the database compellingly. Again it's up to you ;)
+                if (jwtTokenUtil.validateToken(authToken, userDetails)) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    logger.info("authorizated user '{}', setting security context", username);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
-        }
 
-        chain.doFilter(request, response);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- NGPuppies/src/main/java/com/paymentsystem/ngpuppies/web/restControllers/MethodProtectedRestController.java
+++ NGPuppies/src/main/java/com/paymentsystem/ngpuppies/web/restControllers/MethodProtectedRestController.java
@@ -1,11 +1,20 @@
-package com.paymentsystem.ngpuppies.security.controller;
+package com.paymentsystem.ngpuppies.web.restControllers;
 
+import com.paymentsystem.ngpuppies.models.users.Admin;
+import com.paymentsystem.ngpuppies.models.users.ApplicationUser;
+import com.paymentsystem.ngpuppies.security.JwtTokenUtil;
+import com.paymentsystem.ngpuppies.services.AdminServiceImpl;
+import com.paymentsystem.ngpuppies.viewModels.AdminViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("protected")
@@ -16,10 +25,25 @@ public class MethodProtectedRestController {
      * in @PreAuthorize such as 'hasRole()' to determine if a user has access. Remember that the hasRole expression assumes a
      * 'ROLE_' prefix on all role names. So 'ADMIN' here is actually stored as 'ROLE_ADMIN' in database!
      **/
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAdminProtectedGreeting() {
         return ResponseEntity.ok("Greetings from admin protected method!");
+    }
+
+    @GetMapping("/admin/profile")
+    @PreAuthorize("hasRole('ADMIN')")
+    public AdminViewModel getAdminProfile(Authentication authentication) {
+        String username = authentication.getName();
+        Admin admin = adminService.getByUsername(username);
+        
+        return AdminViewModel.fromModel(admin);
     }
 
     @GetMapping("/client")
