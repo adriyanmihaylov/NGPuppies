fb1cda11ad20ff63932a92ebbd89ec63e4fa64ca
qdrenchev <jany.drenchev@gmail.com>
2018-09-06 12:49:27 +0300

    added subscriber view for clients

--- NGPuppies/src/main/java/com/paymentsystem/ngpuppies/web/GeneralController.java
+++ NGPuppies/src/main/java/com/paymentsystem/ngpuppies/web/GeneralController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_CLIENT')")
 @RequestMapping("${common.basepath}/invoice")
 public class GeneralController {
-
+    
 }
---
+++ NGPuppies/src/main/resources/static/app/controllers/subscriber.js
@@ -0,0 +1,63 @@
+app.controller('subscribersController', function($http, $scope) {
+    $scope.subscriber = null;
+    $scope.money = '';
+    $scope.search = function (subscriberPhone) {
+        $scope.subscriber = null;
+        $http({
+            url: 'api/subscriber/' + subscriberPhone,
+            method: "GET",
+            dataType: "json"
+        }).success(function (result) {
+            console.log(result);
+            if (result!=="") {
+                $scope.error = "";
+                $scope.subscriber = result;
+            }else{
+                $scope.error = "Error";
+            }
+        }).error(function (err) {
+            $scope.message = err.message;
+        })
+    };
+    $scope.report = function (subscriberPhone) {
+        var startDate = $("#startDate").val();
+        var endDate = $("#endDate").val();
+        var isPayment = true;
+        $http({
+            url: 'api/subscriber/' + subscriberPhone + '/average?from=' + startDate + "&to=" +endDate,
+            method: "GET",
+            dataType: "json"
+        }).success(function (result) {
+            if (result!==0.0) {
+                console.log(result);
+               $scope.average = result;
+               isPayment = true;
+            }else{
+                isPayment = false;
+            }
+        }).error(function (err) {
+        })
+        $http({
+            url: 'api/subscriber/' + subscriberPhone + '/invoices/max?from=' + startDate + "&to="+endDate,
+            method: "GET",
+            dataType: "json"
+        }).success(function (result) {
+            if (result.id!==0) {
+                console.log(result);
+                $scope.maximum = result.amount;
+                isPayment = true;
+            }else{
+                isPayment = false;
+            }
+            if (isPayment===true){
+                $scope.money = "true";
+            }else{
+                $scope.money  = null;
+            }
+        }).error(function (err) {
+
+        });
+
+    }
+
+});
